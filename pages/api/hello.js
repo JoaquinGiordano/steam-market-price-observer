4a5bc00acca0957789bde255f6b25bf56f5b4ccf
JoaquinGiordano <joaquin_giordano@outlook.com>
2021-04-18 19:05:35 -0300

    Deleting unused files

--- pages/api/hello.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default (req, res) => {
-    res.status(200).json({ name: "John Doe" })
-}
-//https://steamcommunity.com/market/priceoverview/?appid=730&currency=34&market_hash_name=Sticker%20%7C%20Natus%20Vincere%20%28Gold%29%20%7C%202020%20RMR
